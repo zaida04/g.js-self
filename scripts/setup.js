8514b1efec5f8cf5e5b4a0910e530c555dde9353
Nico <nico.03727+github@gmail.com>
2021-07-19 16:23:10 -0400

    refactor(scripts): setup script

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,20 +1,4 @@
-const { exec } = require('child_process');
-const logExecOutput = data => console.log(data);
+const { execSync } = require('child_process');
 
-const runNPMInstall = exec('cd .. && npm i');
-runNPMInstall.stdout.on('data', logExecOutput);
-runNPMInstall.stderr.on('data', logExecOutput);
-runNPMInstall.on('close', () => {
-    const runBootstrap = exec('cd .. && npm run bootstrap');
-    runBootstrap.stdout.on('data', logExecOutput);
-    runBootstrap.stderr.on('data', logExecOutput);
-    runBootstrap.on('close', () => {
-        const runBuild = exec('cd .. && npm run build');
-        runBuild.stdout.on('data', logExecOutput);
-        runBuild.stderr.on('data', logExecOutput);
-        runBuild.on('close', () => {
-            console.log('Setup complete.');
-            process.exit();
-        });
-    });
-});
+const commands = ['cd .. && npm i', 'cd .. && npm run bootstrap', 'cd .. && npm run build'];
+commands.forEach(command => execSync(command));
