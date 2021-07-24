5571ce787f23eaf2a920e19c31e118b07ca75397
Nico <nico.03727+github@gmail.com>
2021-07-24 19:47:33 -0400

    chore(common): cleanup GuildedAPIError

--- packages/rest/lib/GuildedAPIError.ts
+++ packages/rest/lib/GuildedAPIError.ts
@@ -1,6 +1,5 @@
 export class GuildedAPIError extends Error {
     public constructor(msg: string, method: string, path: string, code: number | string) {
         super(`[GuildedAPIError:${code}:${method.toUpperCase()}] ${path} - ${msg}`);
-        this.message = `[GuildedAPIError:${code}:${method.toUpperCase()}] ${path} - ${msg}`;
     }
 }
