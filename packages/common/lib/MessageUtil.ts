357e50f867abe8896a0f4dc5b176565d9fff54ea
Nico <nico.03727+github@gmail.com>
2021-08-05 20:02:03 -0400

    Revert "refactor: remove unneeded color func"
    This reverts commit c6d2524485ffbfbfe58145daa956a1220817d5cc.

--- packages/common/lib/MessageUtil.ts
+++ packages/common/lib/MessageUtil.ts
@@ -260,3 +260,26 @@ export interface enforcedMessageStructure {
     messageId: string;
     content: APIContent;
 }
+
+/**
+ * Copyright 2015 - 2021 Amish Shah
+ * Copyrights licensed under the Apache License 2.0, https://github.com/discordjs/discord.js/blob/master/LICENSE
+ * Taken from https://github.com/discordjs/discord.js/blob/stable/src/util/Util.js#L436
+ */
+export function resolveColor(color: string | number | [number, number, number]): number {
+    let resolvedColor;
+
+    if (typeof color === 'string') {
+        if (color === 'RANDOM') return Math.floor(Math.random() * (0xffffff + 1));
+        if (color === 'DEFAULT') return 0;
+        resolvedColor = CONSTANTS.COLORS[color] || parseInt(color.replace('#', ''), 16);
+    } else if (Array.isArray(color)) {
+        color = (color[0] << 16) | (color[1] << 8) | color[2];
+    }
+
+    if (color < 0 || color > 0xffffff) throw new RangeError('COLOR_RANGE');
+    else if (color && isNaN(color as number)) throw new TypeError('COLOR_CONVERT');
+    else resolvedColor = color as number;
+
+    return resolvedColor;
+}
